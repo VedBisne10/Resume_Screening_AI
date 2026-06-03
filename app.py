86e9cb842eb5781dc317b47550df8ce50886b9ae
VedBisne10 <vedbisne@gmail.com>
2026-06-03 20:47:04 +0530

    Added code comments and documentation

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from src.parser.pdf_parser import extract_text_from_pdf
+from src.parser.pdf_parser import extract_text_from_pdf         # Importing the text extraction function from parser folder
 
-pdf_path = "data/resumes/Resume.pdf"
-text = extract_text_from_pdf(pdf_path)
-print(text)
+pdf_path = "data/resumes/Resume.pdf"                    # Specifying which resume's text should be extracted
+text = extract_text_from_pdf(pdf_path)                  # Calling the function of text extraction
+print(text)                                             # Printing the extracted text
--- src/parser/pdf_parser.py
+++ src/parser/pdf_parser.py
@@ -1,18 +1,18 @@
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader     # Importing PyPDF2 for reading and extracting texts from PDF.
 
-def extract_text_from_pdf(pdf_path: str) -> str:
+def extract_text_from_pdf(pdf_path: str) -> str:       # Function for text extraction with type hints
     try:
-        reader = PdfReader(pdf_path)
-        extracted_text = ""
+        reader = PdfReader(pdf_path)        # Opens the pdf file using pdfReader
+        extracted_text = ""                 # Creates an empty string to store text extracted from all the pages
 
-        for page in reader.pages:
-            page_text = page.extract_text()
+        for page in reader.pages:           # Loop through every page in the pdf
+            page_text = page.extract_text()     # Extract text from the current page
 
-            if page_text:
-                extracted_text += page_text + "\n"
+            if page_text:        # Check if any text is extracted from the pdf
+                extracted_text += page_text + "\n"       # Adds extracted page text to the final text and '\n' adds a new line after each page 
         
-        return extracted_text
+        return extracted_text       # Returns the complete extracted text from pdf
 
-    except Exception as e:
+    except Exception as e:          # Handles any error that occur while reading the pdf
         print(f"Error reading PDF: {e}")
-        return ""
+        return ""               # Returns empty string if extraction fails.
